fbc6179439a85595e1447e688f3fd7b9ef927f56
vikasmaliya004 <vikasmaliya004@gmail.com>
2022-06-11 10:23:04 +0530

    hy]

---
+++ .vscode/mock2.js
@@ -0,0 +1,10 @@
+ //function statment
+ let result = function() {
+     console.log("Hello vikas");
+ }
+
+ //function exprection
+ function myfunction() {
+     console.log("Hello vikas");
+ }
+ myfunction();
